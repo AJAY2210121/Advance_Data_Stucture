8ca36494d462dea000b2f27f5d3d85a654a3515e
Ajay kumar yadav <127116097+AJAY2210121@users.noreply.github.com>
2024-10-11 15:24:14 +0530

    Update print1ToNAfterCall.cpp
    Function modification.

--- Recursion/print1ToNAfterCall.cpp
+++ Recursion/print1ToNAfterCall.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int print1ToN(int n){
-    
+   if(n == 0) return 0;  // Base condition
+    print1ToN(n-1); // funtion call
+    cout<< n << endl; // output
 }
 int main(){
     int n;
